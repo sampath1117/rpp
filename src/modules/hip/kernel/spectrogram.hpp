2d6a8234023d89386c790417b8d32bee68129945
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2022-11-14 18:22:40 -0800

    1) Remove Hannwindow and create separate kernel for Hannwindow_specgram 2) Removed unwanted malloc and memcpy for windowoutput 3) Removed commented time code

--- src/modules/hip/kernel/spectrogram.hpp
+++ src/modules/hip/kernel/spectrogram.hpp
@@ -1,19 +1,6 @@
 #include <hip/hip_runtime.h>
 #include "rpp_hip_common.hpp"
 
-void HannWindow_hip(float *output, int N) {
-  double a = (2 * M_PI / N);
-  for (int t = 0; t < N; t++) {
-    double phase = a * (t + 0.5);
-    output[t] = (0.5 * (1.0 - std::cos(phase)));
-  }
-}
-
-__device__ int getOutputSize_hip(int length, int windowLength, int windowStep, bool centerWindows) {
-    if (!centerWindows)
-        length -= windowLength;
-    return ((length / windowStep) + 1);
-}
 
 __device__ int getIdxReflect(int idx, int lo, int hi) {
     if (hi - lo < 2)
@@ -73,6 +60,59 @@ __global__ void spectrogram_tensor(float *srcPtr,
             } else {
                 if (inIdx >= 0 && inIdx < bufferLength) {
                     *windowOutputTemp++ = windowFunction[t] * srcPtrTemp[inIdx];
+                } else {
+                    *windowOutputTemp++ = 0;
+                }
+            }
+        }
+    }
+}
+
+__global__ void spectrogram_tensor_HannWindow(float *srcPtr,
+                                                uint2 srcStridesNH,
+                                                int *srcLengthTensor,
+                                                int maxNumWindow,
+                                                int numSamples,
+                                                int windowCenterOffset,
+                                                bool reflectPadding,
+                                                float *windowOutput,
+                                                int windowLength,
+                                                int windowStep,
+                                                bool centerWindows)
+{
+    int id_x = (hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x);
+    int id_z = (hipBlockIdx_z * hipBlockDim_z + hipThreadIdx_z);
+
+    double a = (2 * PI / windowLength);
+
+    if (id_z >= numSamples)
+        return;
+
+    int numWindow =  ((!centerWindows) ? (srcLengthTensor[id_z] - windowLength) : (srcLengthTensor[id_z])) / windowStep + 1;
+    
+    if (id_x >= numWindow)
+        return;
+
+    float *srcPtrTemp = srcPtr + id_z * srcStridesNH.x;
+    int bufferLength = srcLengthTensor[id_z];
+
+    float* windowOutputTemp = windowOutput + (id_z * windowLength * maxNumWindow) + (id_x * windowLength);
+    int64_t windowStart = (id_x * windowStep) - windowCenterOffset;
+    if (windowStart >= 0 && (windowStart + windowLength) < bufferLength) {
+        for (int t = 0; t < windowLength; t++) {
+            int64_t inIdx = windowStart + t;
+            *windowOutputTemp++ = (0.5 * (1.0 - cosf(a * (t + 0.5)))) * srcPtrTemp[inIdx];
+        }
+    } else {
+        for (int t = 0; t < windowLength; t++) {
+            int64_t inIdx = windowStart + t;
+
+            if (reflectPadding) {
+                inIdx = getIdxReflect(inIdx, 0, bufferLength);
+                *windowOutputTemp++ = (0.5 * (1.0 - cosf(a * (t + 0.5)))) * srcPtrTemp[inIdx];
+            } else {
+                if (inIdx >= 0 && inIdx < bufferLength) {
+                    *windowOutputTemp++ = (0.5 * (1.0 - cosf(a * (t + 0.5)))) * srcPtrTemp[inIdx];
                 } else {
                     *windowOutputTemp++ = 0;
                 }
@@ -147,7 +187,6 @@ RppStatus hip_exec_spectrogram_tensor(Rpp32f *srcPtr,
                                   RpptSpectrogramLayout layout,
                                   rpp::Handle& handle)
 {
-    // auto t_start_1 = std::chrono::high_resolution_clock::now();
     int maxNumWindow = -1;
 
     for (int batchCount = 0; batchCount < srcDescPtr->n; batchCount++)
@@ -155,6 +194,24 @@ RppStatus hip_exec_spectrogram_tensor(Rpp32f *srcPtr,
         maxNumWindow = std::max(maxNumWindow,  ((!centerWindows) ? (srcLengthTensor[batchCount] - windowLength) : (srcLengthTensor[batchCount])) / windowStep + 1);
     }
 
+    bool vertical = (layout == RpptSpectrogramLayout::FT);
+    int windowCenterOffset = 0;
+
+    if (centerWindows)
+        windowCenterOffset = windowLength / 2;
+
+    if(nfft == 0.0f)
+        nfft = windowLength;
+
+    int numBins = nfft / 2 + 1;
+    
+    float* d_windowOutput;
+    hipMalloc(&d_windowOutput, srcDescPtr->n * maxNumWindow * windowLength * sizeof(float));
+    
+    int *d_srcLengthTensor;
+    hipMalloc(&d_srcLengthTensor, srcDescPtr->n * sizeof(int));
+    hipMemcpy(d_srcLengthTensor, srcLengthTensor, srcDescPtr->n * sizeof(int), hipMemcpyHostToDevice);
+    
     int localThreads_x = LOCAL_THREADS_X;
     int localThreads_y = LOCAL_THREADS_Z;
     int localThreads_z = LOCAL_THREADS_Y;
@@ -162,61 +219,42 @@ RppStatus hip_exec_spectrogram_tensor(Rpp32f *srcPtr,
     int globalThreads_y = 1;
     int globalThreads_z = srcDescPtr->n;
 
-
-    bool vertical = (layout == RpptSpectrogramLayout::FT);
-    int windowCenterOffset = 0;
-
-    if (centerWindows)
-        windowCenterOffset = windowLength / 2;
-
-    // auto t_start_2 = std::chrono::high_resolution_clock::now();
     if (windowFunction == NULL) {
-        // TODO: clear when necessary
-       windowFunction = (float*)calloc(windowLength,sizeof(float));
-       HannWindow_hip(windowFunction, windowLength);
+        hipLaunchKernelGGL(spectrogram_tensor_HannWindow,
+                        dim3(ceil((float)globalThreads_x/localThreads_x), ceil((float)globalThreads_y/localThreads_y), ceil((float)globalThreads_z/localThreads_z)),
+                        dim3(localThreads_x, localThreads_y, localThreads_z),
+                        0,
+                        handle.GetStream(),
+                        srcPtr,
+                        make_uint2(srcDescPtr->strides.nStride, srcDescPtr->strides.hStride),
+                        d_srcLengthTensor,
+                        maxNumWindow,
+                        srcDescPtr->n,
+                        windowCenterOffset,
+                        reflectPadding,
+                        d_windowOutput,
+                        windowLength,
+                        windowStep,
+                        centerWindows);
+    } else {
+        hipLaunchKernelGGL(spectrogram_tensor,
+                        dim3(ceil((float)globalThreads_x/localThreads_x), ceil((float)globalThreads_y/localThreads_y), ceil((float)globalThreads_z/localThreads_z)),
+                        dim3(localThreads_x, localThreads_y, localThreads_z),
+                        0,
+                        handle.GetStream(),
+                        srcPtr,
+                        make_uint2(srcDescPtr->strides.nStride, srcDescPtr->strides.hStride),
+                        d_srcLengthTensor,
+                        maxNumWindow,
+                        srcDescPtr->n,
+                        windowCenterOffset,
+                        reflectPadding,
+                        windowFunction,
+                        d_windowOutput,
+                        windowLength,
+                        windowStep,
+                        centerWindows);
     }
-    // auto t_end_2 = std::chrono::high_resolution_clock::now();
-
-    if(nfft == 0.0f)
-        nfft = windowLength;
-
-    int numBins = nfft / 2 + 1;
-
-    float* windowOutput = (float*)calloc(srcDescPtr->n * maxNumWindow * windowLength,(sizeof(float)));
-
-    // auto t_start_3 = std::chrono::high_resolution_clock::now();
-    // auto t_end_3 = std::chrono::high_resolution_clock::now();
-
-    // auto t_start_4 = std::chrono::high_resolution_clock::now();
-    float* d_windowOutput, *d_windowFunction;
-    hipMalloc(&d_windowOutput, srcDescPtr->n * maxNumWindow * windowLength * sizeof(float));
-    hipMalloc(&d_windowFunction,  windowLength * sizeof(float));
-    hipMemcpy(d_windowOutput, windowOutput, srcDescPtr->n * maxNumWindow * windowLength * sizeof(float), hipMemcpyHostToDevice);
-    hipMemcpy(d_windowFunction, windowFunction, windowLength * sizeof(float), hipMemcpyHostToDevice);
-
-    int *d_srcLengthTensor;
-    hipMalloc(&d_srcLengthTensor, srcDescPtr->n * sizeof(int));
-    hipMemcpy(d_srcLengthTensor, srcLengthTensor, srcDescPtr->n * sizeof(int), hipMemcpyHostToDevice);
-    // auto t_end_4 = std::chrono::high_resolution_clock::now();
-
-    // auto t_start_5 = std::chrono::high_resolution_clock::now();
-    hipLaunchKernelGGL(spectrogram_tensor,
-                       dim3(ceil((float)globalThreads_x/localThreads_x), ceil((float)globalThreads_y/localThreads_y), ceil((float)globalThreads_z/localThreads_z)),
-                       dim3(localThreads_x, localThreads_y, localThreads_z),
-                       0,
-                       handle.GetStream(),
-                       srcPtr,
-                       make_uint2(srcDescPtr->strides.nStride, srcDescPtr->strides.hStride),
-                       d_srcLengthTensor,
-                       maxNumWindow,
-                       srcDescPtr->n,
-                       windowCenterOffset,
-                       reflectPadding,
-                       d_windowFunction,
-                       d_windowOutput,
-                       windowLength,
-                       windowStep,
-                       centerWindows);
     hipDeviceSynchronize();
 
     localThreads_x = FFT_LOCAL_THREADS_X;
@@ -247,38 +285,8 @@ RppStatus hip_exec_spectrogram_tensor(Rpp32f *srcPtr,
                        centerWindows);
     hipDeviceSynchronize();
 
-
-    // auto t_end_5 = std::chrono::high_resolution_clock::now();
-
-    // auto t_start_6 = std::chrono::high_resolution_clock::now();
-    // auto t_start_61 = std::chrono::high_resolution_clock::now();
-    hipFree(d_windowFunction);
     hipFree(d_windowOutput);
     hipFree(d_srcLengthTensor);
-    // auto t_end_61 = std::chrono::high_resolution_clock::now();
-    // auto t_start_62 = std::chrono::high_resolution_clock::now();
-    free(windowFunction);
-    free(windowOutput);
-    // auto t_end_62 = std::chrono::high_resolution_clock::now();
-    // auto t_end_6 = std::chrono::high_resolution_clock::now();
-    // auto t_end_1 = std::chrono::high_resolution_clock::now();
-    // // std::cout << "\nTimer 1 (total time): " << std::chrono::duration<double, std::milli>(t_end_1-t_start_1).count()
-    //           << " ms\n";
-    // std::cout << "\nTimer 2 (hannwindow): " << std::chrono::duration<double, std::milli>(t_end_2-t_start_2).count()
-    //           << " ms\n";
-    // std::cout << "\nTimer 3 (cos and sin calculation): " << std::chrono::duration<double, std::milli>(t_end_3-t_start_3).count()
-    //           << " ms\n";
-    // std::cout << "\nTimer 4 (memcpy and malloc): " << std::chrono::duration<double, std::milli>(t_end_4-t_start_4).count()
-    //           << " ms\n";
-    // std::cout << "\nTimer 5 (kernel launch):" << std::chrono::duration<double, std::milli>(t_end_5-t_start_5).count()
-    //           << " ms\n";
-    // std::cout << "\nTimer 6 (free): " << std::chrono::duration<double, std::milli>(t_end_6-t_start_6).count()
-    //           << " ms\n";
-    // std::cout << "\nTimer 61 (device free): " << std::chrono::duration<double, std::milli>(t_end_61-t_start_61).count()
-    //           << " ms\n";
-    // std::cout << "\nTimer 62 (Host free): " << std::chrono::duration<double, std::milli>(t_end_62-t_start_62).count()
-    //           << " ms\n";
-
 
 
     return RPP_SUCCESS;
