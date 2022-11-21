596c6f37c8c81d5c4bb1f197a89584a12fe051be
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2022-11-21 06:02:44 -0800

    Internal batch size updation

--- src/modules/cl/handleocl.cpp
+++ src/modules/cl/handleocl.cpp
@@ -25,6 +25,7 @@
  *******************************************************************************/
 
 #include <boost/filesystem.hpp>
+#include<thread>
 #ifndef _WIN32
 #include <unistd.h>
 #endif
@@ -260,6 +261,7 @@ struct HandleImpl
     bool enable_profiling = false;
     float profiling_result = 0.0;
     size_t nBatchSize = 1;
+    size_t internalBatchSize;
     InitHandle* initHandle = nullptr;
 
     ContextPtr create_context()
@@ -345,6 +347,50 @@ struct HandleImpl
             clGetEventProfilingInfo(e, CL_PROFILING_COMMAND_END, sizeof(size_t), &end, nullptr);
             profiling_result = static_cast<float>(end - st) * 1.0e-6; // NOLINT
         }
+    }
+
+    uint compute_internal_batch_size(Rpp32u user_batch_size)
+    {
+      const unsigned MINIMUM_CPU_THREAD_COUNT = 2;
+      const unsigned DEFAULT_SMT_COUNT = 2;
+      unsigned THREAD_COUNT = std::thread::hardware_concurrency();
+      // if(THREAD_COUNT >= MINIMUM_CPU_THREAD_COUNT)
+      // {
+      //     INFO("Can run " + TOSTR(THREAD_COUNT) + " threads simultaneously on this machine")
+      // }
+      if(THREAD_COUNT < MINIMUM_CPU_THREAD_COUNT)
+      {
+          THREAD_COUNT = MINIMUM_CPU_THREAD_COUNT;
+          std::cerr<<"hardware_concurrency() call failed assuming can run "<<THREAD_COUNT<<" threads";
+      }
+      size_t ret = user_batch_size;
+      size_t CORE_COUNT = THREAD_COUNT / DEFAULT_SMT_COUNT;
+
+      if(CORE_COUNT <= 0)
+      {
+          std::cerr<<"\n Wrong core count detected less than 0";
+          exit(0);
+      }
+
+      for( size_t i = CORE_COUNT; i <= THREAD_COUNT; i++)
+      {
+        if(user_batch_size % i == 0)
+            {
+                ret = i;
+                break;
+            }
+        }
+
+        for(size_t i = CORE_COUNT; i > 1; i--)
+        {
+        if(user_batch_size % i == 0)
+            {
+                    ret = i;
+                break;
+            }
+        }
+        // std::cerr<<"\n User batch size "<<(user_batch_size)<<" Internal batch size set to "<<ret;
+        return ret;
     }
 
     void PreInitializeBufferCPU()
@@ -447,6 +493,7 @@ Handle::Handle(size_t batchSize) : impl(new HandleImpl())
     impl->nBatchSize = batchSize;
     this->SetAllocator(nullptr, nullptr, nullptr);
     impl->PreInitializeBufferCPU();
+    impl->internalBatchSize = impl->compute_internal_batch_size(impl->nBatchSize);
 }
 
 Handle::Handle() : impl(new HandleImpl())
@@ -605,6 +652,11 @@ void Handle::rpp_destroy_object_host()
 size_t Handle::GetBatchSize() const
 {
     return this->impl->nBatchSize;
+}
+
+size_t Handle::GetInternalBatchSize() const 
+{
+    return this->impl->internalBatchSize;
 }
 
 void Handle::SetBatchSize(size_t bSize) const
