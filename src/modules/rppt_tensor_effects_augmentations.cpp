d0f6396bdff858e11bd30f21232194b7b04819e5
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2023-01-04 00:22:19 -0800

    added support for I8, F32, F16 bitdepths for water HOST

--- src/modules/rppt_tensor_effects_augmentations.cpp
+++ src/modules/rppt_tensor_effects_augmentations.cpp
@@ -515,42 +515,54 @@ RppStatus rppt_water_host(RppPtr_t srcPtr,
                                 roiType,
                                 layoutParams);
     }
-    // else if ((srcDescPtr->dataType == RpptDataType::F16) && (dstDescPtr->dataType == RpptDataType::F16))
-    // {
-    //     water_f16_f16_host_tensor((Rpp16f*) (static_cast<Rpp8u*>(srcPtr) + srcDescPtr->offsetInBytes),
-    //                                    srcDescPtr,
-    //                                    (Rpp16f*) (static_cast<Rpp8u*>(dstPtr) + dstDescPtr->offsetInBytes),
-    //                                    dstDescPtr,
-    //                                    alphaTensor,
-    //                                    betaTensor,
-    //                                    roiTensorPtrSrc,
-    //                                    roiType,
-    //                                    layoutParams);
-    // }
-    // else if ((srcDescPtr->dataType == RpptDataType::F32) && (dstDescPtr->dataType == RpptDataType::F32))
-    // {
-    //     water_f32_f32_host_tensor((Rpp32f*) (static_cast<Rpp8u*>(srcPtr) + srcDescPtr->offsetInBytes),
-    //                                    srcDescPtr,
-    //                                    (Rpp32f*) (static_cast<Rpp8u*>(dstPtr) + dstDescPtr->offsetInBytes),
-    //                                    dstDescPtr,
-    //                                    alphaTensor,
-    //                                    betaTensor,
-    //                                    roiTensorPtrSrc,
-    //                                    roiType,
-    //                                    layoutParams);
-    // }
-    // else if ((srcDescPtr->dataType == RpptDataType::I8) && (dstDescPtr->dataType == RpptDataType::I8))
-    // {
-    //     water_i8_i8_host_tensor(static_cast<Rpp8s*>(srcPtr) + srcDescPtr->offsetInBytes,
-    //                                  srcDescPtr,
-    //                                  static_cast<Rpp8s*>(dstPtr) + dstDescPtr->offsetInBytes,
-    //                                  dstDescPtr,
-    //                                  alphaTensor,
-    //                                  betaTensor,
-    //                                  roiTensorPtrSrc,
-    //                                  roiType,
-    //                                  layoutParams);
-    // }
+    else if ((srcDescPtr->dataType == RpptDataType::F16) && (dstDescPtr->dataType == RpptDataType::F16))
+    {
+        water_f16_f16_host_tensor((Rpp16f*) (static_cast<Rpp8u*>(srcPtr) + srcDescPtr->offsetInBytes),
+                                  srcDescPtr,
+                                  (Rpp16f*) (static_cast<Rpp8u*>(dstPtr) + dstDescPtr->offsetInBytes),
+                                  dstDescPtr,
+                                  amplitudeXTensor,
+                                  amplitudeYTensor,
+                                  frequencyXTensor,
+                                  frequencyYTensor,
+                                  phaseXTensor,
+                                  phaseYTensor,
+                                  roiTensorPtrSrc,
+                                  roiType,
+                                  layoutParams);
+    }
+    else if ((srcDescPtr->dataType == RpptDataType::F32) && (dstDescPtr->dataType == RpptDataType::F32))
+    {
+        water_f32_f32_host_tensor((Rpp32f*) (static_cast<Rpp8u*>(srcPtr) + srcDescPtr->offsetInBytes),
+                                  srcDescPtr,
+                                  (Rpp32f*) (static_cast<Rpp8u*>(dstPtr) + dstDescPtr->offsetInBytes),
+                                  dstDescPtr,
+                                  amplitudeXTensor,
+                                  amplitudeYTensor,
+                                  frequencyXTensor,
+                                  frequencyYTensor,
+                                  phaseXTensor,
+                                  phaseYTensor,
+                                  roiTensorPtrSrc,
+                                  roiType,
+                                  layoutParams);
+    }
+    else if ((srcDescPtr->dataType == RpptDataType::I8) && (dstDescPtr->dataType == RpptDataType::I8))
+    {
+        water_i8_i8_host_tensor(static_cast<Rpp8s*>(srcPtr) + srcDescPtr->offsetInBytes,
+                                srcDescPtr,
+                                static_cast<Rpp8s*>(dstPtr) + dstDescPtr->offsetInBytes,
+                                dstDescPtr,
+                                amplitudeXTensor,
+                                amplitudeYTensor,
+                                frequencyXTensor,
+                                frequencyYTensor,
+                                phaseXTensor,
+                                phaseYTensor,
+                                roiTensorPtrSrc,
+                                roiType,
+                                layoutParams);
+    }
 
     return RPP_SUCCESS;
 }
