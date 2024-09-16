50859a934c098373620935dffa7c0823071431b0
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2024-09-16 15:16:57 +0000

    added initial code for fishey src loc compute and support for U8 PKD3-PLN3 variant

--- src/modules/cpu/kernel/fisheye.hpp
+++ src/modules/cpu/kernel/fisheye.hpp
@@ -26,6 +26,54 @@ SOFTWARE.
 #include "rpp_cpu_simd.hpp"
 #include "rpp_cpu_common.hpp"
 
+inline void compute_fisheye_src_loc_avx(__m256 &pDstY, __m256 &pDstX, __m256 &pSrcY, __m256 &pSrcX, __m256 &pHeight, __m256 &pWidth)
+{
+    __m256 pNormX, pNormY, pDist;
+    pNormX = _mm256_sub_ps(_mm256_div_ps(_mm256_mul_ps(avx_p2, pDstX), pWidth), avx_p1);        //  (static_cast<Rpp32f>((2.0 * dstX)) / width) - 1;
+    pNormY = _mm256_sub_ps(_mm256_div_ps(_mm256_mul_ps(avx_p2, pDstY), pHeight), avx_p1);       //  (static_cast<Rpp32f>((2.0 * dstY)) / height) - 1;
+    pDist = _mm256_sqrt_ps(_mm256_fmadd_ps(pNormX, pNormX, _mm256_mul_ps(pNormY, pNormY)));     //  std::sqrt((normX * normX) + (normY * normY));
+    
+    __m256 pDistNew, pTheta, pSinFactor, pCosFactor;
+    pDistNew = _mm256_sqrt_ps(_mm256_sub_ps(avx_p1, _mm256_mul_ps(pDist, pDist)));              //  std::sqrt(1.0 - dist * dist);
+    pDistNew = _mm256_mul_ps(_mm256_add_ps(pDist, _mm256_sub_ps(avx_p1, pDistNew)), avx_p1op2); //  (dist + (1.0 - distNew)) * 0.5f; 
+    pTheta = atan2_ps(pNormY, pNormX);                                                          //  std::atan2(normY, normX);
+    sincos_ps(pTheta, &pSinFactor, &pCosFactor);
+
+    pSrcX = _mm256_mul_ps(_mm256_mul_ps(_mm256_fmadd_ps(pDistNew, pCosFactor, avx_p1), pWidth), avx_p1op2);
+    pSrcY = _mm256_mul_ps(_mm256_mul_ps(_mm256_fmadd_ps(pDistNew, pSinFactor, avx_p1), pHeight), avx_p1op2);
+    pSrcX = _mm256_blendv_ps(avx_pMinus1, pSrcX, _mm256_cmp_ps(pSrcX, pSrcX, _CMP_ORD_Q));
+    pSrcY = _mm256_blendv_ps(avx_pMinus1, pSrcY, _mm256_cmp_ps(pSrcY, pSrcY, _CMP_ORD_Q));
+
+    __m256 pMask1, pMask2;
+    pMask1 = _mm256_and_ps(_mm256_cmp_ps(pDist, avx_p0, _CMP_GE_OQ), _mm256_cmp_ps(pDist, avx_p1, _CMP_LE_OQ));
+    pMask2 = _mm256_and_ps(pMask1, _mm256_cmp_ps(pDistNew, avx_p1, _CMP_LE_OQ));
+    pSrcX = _mm256_blendv_ps(avx_pMinus1, pSrcX, pMask2);
+    pSrcY = _mm256_blendv_ps(avx_pMinus1, pSrcY, pMask2);
+    pDstX = _mm256_add_ps(pDstX, avx_p8);
+}
+
+inline void compute_fisheye_src_loc(Rpp32f dstY, Rpp32f dstX, Rpp32f &srcY, Rpp32f &srcX, Rpp32s &height, Rpp32s &width)
+{
+    Rpp32f normX = (static_cast<Rpp32f>((2.0 * dstX)) / width) - 1;
+    Rpp32f normY = (static_cast<Rpp32f>((2.0 * dstY)) / height) - 1;
+    Rpp32f dist = std::sqrt((normX * normX) + (normY * normY));
+    srcX = -1;
+    srcY = -1;
+    if ((dist >= 0.0) && (dist <= 1.0))
+    {
+        Rpp32f distNew = std::sqrt(1.0 - dist * dist);
+        distNew = (dist + (1.0 - distNew)) * 0.5f;
+        if (distNew <= 1.0)
+        {
+            Rpp32f theta = std::atan2(normY, normX);
+            Rpp32f newX = distNew * std::cos(theta);
+            Rpp32f newY = distNew * std::sin(theta);
+            srcX = (((newX + 1) * width) * 0.5f);
+            srcY = (((newY + 1) * height) * 0.5f);
+        }
+    }
+}
+
 RppStatus fisheye_u8_u8_host_tensor(Rpp8u *srcPtr,
                                     RpptDescPtr srcDescPtr,
                                     Rpp8u *dstPtr,
@@ -42,61 +90,78 @@ RppStatus fisheye_u8_u8_host_tensor(Rpp8u *srcPtr,
 #pragma omp parallel for num_threads(numThreads)
     for(int batchCount = 0; batchCount < dstDescPtr->n; batchCount++)
     {
-        RpptROI roi;
+        RpptROI roi, roiLTRB;
         RpptROIPtr roiPtrInput = &roiTensorPtrSrc[batchCount];
         compute_roi_validation_host(roiPtrInput, &roi, &roiDefault, roiType);
+        compute_ltrb_from_xywh_host(&roi, &roiLTRB);
+        Rpp32u bufferLength = roi.xywhROI.roiWidth;
 
         Rpp8u *srcPtrImage, *dstPtrImage;
         srcPtrImage = srcPtr + batchCount * srcDescPtr->strides.nStride;
         dstPtrImage = dstPtr + batchCount * dstDescPtr->strides.nStride;
 
-        Rpp32u bufferLength = roi.xywhROI.roiWidth * layoutParams.bufferMultiplier;
-
         Rpp8u *srcPtrChannel, *dstPtrChannel;
         srcPtrChannel = srcPtrImage + (roi.xywhROI.xy.y * srcDescPtr->strides.hStride) + (roi.xywhROI.xy.x * layoutParams.bufferMultiplier);
         dstPtrChannel = dstPtrImage;
 
+        Rpp32s vectorIncrementPerChannel = 8;
+        Rpp32s vectorIncrementPkd = 24;
+        Rpp32u alignedLength = bufferLength & ~7;   // Align dst width to process 8 dst pixels per iteration
+        Rpp32s srcLocArray[8] = {0};                // Since 8 dst pixels are processed per iteration
+        Rpp32s invalidLoad[8] = {0};                // Since 8 dst pixels are processed per iteration
+
+#if __AVX2__
+        __m256 pSrcStrideH = _mm256_set1_ps(srcDescPtr->strides.hStride);
+        __m256 pRoiLTRB[4], pWidth, pHeight;
+        pRoiLTRB[0] = _mm256_set1_ps(roiLTRB.ltrbROI.lt.x);
+        pRoiLTRB[1] = _mm256_set1_ps(roiLTRB.ltrbROI.lt.y);
+        pRoiLTRB[2] = _mm256_set1_ps(roiLTRB.ltrbROI.rb.x);
+        pRoiLTRB[3] = _mm256_set1_ps(roiLTRB.ltrbROI.rb.y);
+        pWidth = _mm256_set1_ps(roi.xywhROI.roiWidth);
+        pHeight = _mm256_set1_ps(roi.xywhROI.roiHeight);
+#endif
+
         // fisheye with fused output-layout toggle (NHWC -> NCHW)
         if ((srcDescPtr->c == 3) && (srcDescPtr->layout == RpptLayout::NHWC) && (dstDescPtr->layout == RpptLayout::NCHW))
         {
-            Rpp32u alignedLength = (bufferLength / 48) * 48;
-            Rpp8u *srcPtrRow, *dstPtrRowR, *dstPtrRowG, *dstPtrRowB;
-            srcPtrRow = srcPtrChannel;
+            Rpp8u *dstPtrRowR, *dstPtrRowG, *dstPtrRowB;
             dstPtrRowR = dstPtrChannel;
             dstPtrRowG = dstPtrRowR + dstDescPtr->strides.cStride;
             dstPtrRowB = dstPtrRowG + dstDescPtr->strides.cStride;
-
             for(int i = 0; i < roi.xywhROI.roiHeight; i++)
             {
-                Rpp8u *srcPtrTemp, *dstPtrTempR, *dstPtrTempG, *dstPtrTempB;
-                srcPtrTemp = srcPtrRow;
+                Rpp8u *dstPtrTempR, *dstPtrTempG, *dstPtrTempB;
                 dstPtrTempR = dstPtrRowR;
                 dstPtrTempG = dstPtrRowG;
                 dstPtrTempB = dstPtrRowB;
 
-                int vectorLoopCount = 0;
-                for (; vectorLoopCount < alignedLength; vectorLoopCount+=48)
+                Rpp32f dstX, dstY;
+                dstY = static_cast<Rpp32f>(i);
+                Rpp32s vectorLoopCount = 0;
+#if __AVX2__
+                __m256 pDstX, pDstY;
+                pDstX = avx_pDstLocInit;
+                pDstY = _mm256_set1_ps(dstY);
+                for (; vectorLoopCount < alignedLength; vectorLoopCount += vectorIncrementPerChannel)
                 {
-                    __m128i px[3];
-                    rpp_simd_load(rpp_load48_u8pkd3_to_u8pln3, srcPtrTemp, px);    // simd loads
-                    rpp_simd_store(rpp_store48_u8pln3_to_u8pln3, dstPtrTempR, dstPtrTempG, dstPtrTempB, px);    // simd stores
-                    srcPtrTemp += 48;
-                    dstPtrTempR += 16;
-                    dstPtrTempG += 16;
-                    dstPtrTempB += 16;
+                    __m256 pSrcX, pSrcY;
+                    __m256i pRow;
+                    compute_fisheye_src_loc_avx(pDstY, pDstX, pSrcY, pSrcX, pHeight, pWidth);
+                    compute_generic_nn_srclocs_and_validate_avx(pSrcY, pSrcX, pRoiLTRB, pSrcStrideH, srcLocArray, invalidLoad, true);
+                    rpp_simd_load(rpp_generic_nn_load_u8pkd3_avx, srcPtrChannel, srcLocArray, invalidLoad, pRow);
+                    rpp_simd_store(rpp_store24_u8pkd3_to_u8pln3_avx, dstPtrTempR, dstPtrTempG, dstPtrTempB, pRow);
+                    dstPtrTempR += vectorIncrementPerChannel;
+                    dstPtrTempG += vectorIncrementPerChannel;
+                    dstPtrTempB += vectorIncrementPerChannel;
                 }
-                for (; vectorLoopCount < bufferLength; vectorLoopCount+=3)
+#endif
+                for (; vectorLoopCount < bufferLength; vectorLoopCount++)
                 {
-                    *dstPtrTempR = srcPtrTemp[0];
-                    *dstPtrTempG = srcPtrTemp[1];
-                    *dstPtrTempB = srcPtrTemp[2];
-                    srcPtrTemp += 3;
-                    dstPtrTempR++;
-                    dstPtrTempG++;
-                    dstPtrTempB++;
+                    Rpp32f srcX, srcY, cosFactor;
+                    dstX = static_cast<Rpp32f>(vectorLoopCount);
+                    compute_fisheye_src_loc(dstY, dstX, srcY, srcX, roi.xywhROI.roiHeight, roi.xywhROI.roiWidth);
+                    compute_generic_nn_interpolation_pkd3_to_pln3(srcY, srcX, &roiLTRB, dstPtrTempR++, dstPtrTempG++, dstPtrTempB++, srcPtrChannel, srcDescPtr);
                 }
-
-                srcPtrRow += srcDescPtr->strides.hStride;
                 dstPtrRowR += dstDescPtr->strides.hStride;
                 dstPtrRowG += dstDescPtr->strides.hStride;
                 dstPtrRowB += dstDescPtr->strides.hStride;
@@ -112,7 +177,6 @@ RppStatus fisheye_u8_u8_host_tensor(Rpp8u *srcPtr,
             srcPtrRowG = srcPtrRowR + srcDescPtr->strides.cStride;
             srcPtrRowB = srcPtrRowG + srcDescPtr->strides.cStride;
             dstPtrRow = dstPtrChannel;
-
             for(int i = 0; i < roi.xywhROI.roiHeight; i++)
             {
                 Rpp8u *srcPtrTempR, *srcPtrTempG, *srcPtrTempB, *dstPtrTemp;
@@ -120,8 +184,8 @@ RppStatus fisheye_u8_u8_host_tensor(Rpp8u *srcPtr,
                 srcPtrTempG = srcPtrRowG;
                 srcPtrTempB = srcPtrRowB;
                 dstPtrTemp = dstPtrRow;
-
                 int vectorLoopCount = 0;
+#if __AVX2__
                 for (; vectorLoopCount < alignedLength; vectorLoopCount+=16)
                 {
                     __m128i px[3];
@@ -132,6 +196,7 @@ RppStatus fisheye_u8_u8_host_tensor(Rpp8u *srcPtr,
                     srcPtrTempB += 16;
                     dstPtrTemp += 48;
                 }
+#endif
                 for (; vectorLoopCount < bufferLength; vectorLoopCount++)
                 {
                     dstPtrTemp[0] = *srcPtrTempR;
@@ -142,32 +207,10 @@ RppStatus fisheye_u8_u8_host_tensor(Rpp8u *srcPtr,
                     srcPtrTempB++;
                     dstPtrTemp += 3;
                 }
-
                 srcPtrRowR += srcDescPtr->strides.hStride;
                 srcPtrRowG += srcDescPtr->strides.hStride;
                 srcPtrRowB += srcDescPtr->strides.hStride;
                 dstPtrRow += dstDescPtr->strides.hStride;
-            }
-        }
-
-        // fisheye without fused output-layout toggle (NHWC -> NHWC or NCHW -> NCHW)
-        else
-        {
-            for(int c = 0; c < layoutParams.channelParam; c++)
-            {
-                Rpp8u *srcPtrRow, *dstPtrRow;
-                srcPtrRow = srcPtrChannel;
-                dstPtrRow = dstPtrChannel;
-
-                for(int i = 0; i < roi.xywhROI.roiHeight; i++)
-                {
-                    memcpy(dstPtrRow, srcPtrRow, bufferLength);
-                    srcPtrRow += srcDescPtr->strides.hStride;
-                    dstPtrRow += dstDescPtr->strides.hStride;
-                }
-
-                srcPtrChannel += srcDescPtr->strides.cStride;
-                dstPtrChannel += dstDescPtr->strides.cStride;
             }
         }
     }
