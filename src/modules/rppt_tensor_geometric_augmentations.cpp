88e253ec15bf27e052f8425a2ccb350e21aaafc7
sampath1117 <sampath.rachumallu@multicorewareinc.com>
2024-09-25 03:47:06 +0000

    added support for F32 and I8 bitdepths in HOST

--- src/modules/rppt_tensor_geometric_augmentations.cpp
+++ src/modules/rppt_tensor_geometric_augmentations.cpp
@@ -1473,28 +1473,28 @@ RppStatus rppt_fisheye_host(RppPtr_t srcPtr,
                                     layoutParams,
                                     rpp::deref(rppHandle));
     }
-    // else if ((srcDescPtr->dataType == RpptDataType::F32) && (dstDescPtr->dataType == RpptDataType::F32))
-    // {
-    //     fisheye_f32_f32_host_tensor(reinterpret_cast<Rpp32f*>(static_cast<Rpp8u*>(srcPtr) + srcDescPtr->offsetInBytes),
-    //                                 srcDescPtr,
-    //                                 reinterpret_cast<Rpp32f*>(static_cast<Rpp8u*>(dstPtr) + dstDescPtr->offsetInBytes),
-    //                                 dstDescPtr,
-    //                                 roiTensorPtrSrc,
-    //                                 roiType,
-    //                                 layoutParams,
-    //                                 rpp::deref(rppHandle));
-    // }
-    // else if ((srcDescPtr->dataType == RpptDataType::I8) && (dstDescPtr->dataType == RpptDataType::I8))
-    // {
-    //     fisheye_i8_i8_host_tensor(static_cast<Rpp8s*>(srcPtr) + srcDescPtr->offsetInBytes,
-    //                               srcDescPtr,
-    //                               static_cast<Rpp8s*>(dstPtr) + dstDescPtr->offsetInBytes,
-    //                               dstDescPtr,
-    //                               roiTensorPtrSrc,
-    //                               roiType,
-    //                               layoutParams,
-    //                               rpp::deref(rppHandle));
-    // }
+    else if ((srcDescPtr->dataType == RpptDataType::F32) && (dstDescPtr->dataType == RpptDataType::F32))
+    {
+        fisheye_f32_f32_host_tensor(reinterpret_cast<Rpp32f*>(static_cast<Rpp8u*>(srcPtr) + srcDescPtr->offsetInBytes),
+                                    srcDescPtr,
+                                    reinterpret_cast<Rpp32f*>(static_cast<Rpp8u*>(dstPtr) + dstDescPtr->offsetInBytes),
+                                    dstDescPtr,
+                                    roiTensorPtrSrc,
+                                    roiType,
+                                    layoutParams,
+                                    rpp::deref(rppHandle));
+    }
+    else if ((srcDescPtr->dataType == RpptDataType::I8) && (dstDescPtr->dataType == RpptDataType::I8))
+    {
+        fisheye_i8_i8_host_tensor(static_cast<Rpp8s*>(srcPtr) + srcDescPtr->offsetInBytes,
+                                  srcDescPtr,
+                                  static_cast<Rpp8s*>(dstPtr) + dstDescPtr->offsetInBytes,
+                                  dstDescPtr,
+                                  roiTensorPtrSrc,
+                                  roiType,
+                                  layoutParams,
+                                  rpp::deref(rppHandle));
+    }
 
     return RPP_SUCCESS;
 }
