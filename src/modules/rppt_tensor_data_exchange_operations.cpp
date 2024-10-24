06448d8547c918d0c932af3a543ecb31e6b20443
dineshbabu-ravichandran <dineshbabu.ravichandran@multicorewareinc.com>
2024-10-24 13:54:39 +0000

    Minor changes in ptr

--- src/modules/rppt_tensor_data_exchange_operations.cpp
+++ src/modules/rppt_tensor_data_exchange_operations.cpp
@@ -211,11 +211,11 @@ RppStatus rppt_cast_host(RppPtr_t srcPtr,
                                 roiTensor,
                                 rpp::deref(rppHandle));
     }
-    else if ((srcDescPtr->dataType == RpptDataType::I16) && (dstDescPtr->dataType == RpptDataType::F32)) {
-        cast_host_tensor(static_cast<Rpp16s*>(srcPtr) + srcDescPtr->offsetInBytes,
-                         srcGenericDescPtr,,
-                         static_cast<Rpp32f*>(dstPtr) + dstDescPtr->offsetInBytes,
-                         dstGenericDescPtr,,
+    else if ((srcGenericDescPtr->dataType == RpptDataType::I16) && (srcGenericDescPtr->dataType == RpptDataType::F32)) {
+        cast_host_tensor(static_cast<Rpp16s*>(srcPtr) + srcGenericDescPtr->offsetInBytes,
+                         srcGenericDescPtr,
+                         static_cast<Rpp32f*>(dstPtr) + srcGenericDescPtr->offsetInBytes,
+                         dstGenericDescPtr,
                          roiTensor,
                          rpp::deref(rppHandle));
     }
